17b1baac0bca37dbb0153ab333889c14a133272c
Heiko Bornholdt <heiko@bornholdt.io>
2025-04-02 10:01:32 +0200

    Update header references as Apple removes old locations

--- src/main/java/org/drasyl/channel/tun/jna/darwin/IfUtun.java
+++ src/main/java/org/drasyl/channel/tun/jna/darwin/IfUtun.java
@@ -22,7 +22,7 @@
 package org.drasyl.channel.tun.jna.darwin;
 
 /**
- * JNA mapping for <a href="https://opensource.apple.com/source/xnu/xnu-2050.18.24/bsd/net/if_utun.h.auto.html">if_utun.h</a>.
+ * JNA mapping for <a href="https://github.com/apple-oss-distributions/xnu/blob/rel/xnu-6153/bsd/net/if_utun.h">if_utun.h</a>.
  */
 final class IfUtun {
     static final String UTUN_CONTROL_NAME = "com.apple.net.utun_control";
